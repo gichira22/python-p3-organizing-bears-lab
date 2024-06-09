f72ae181f79965761b7765dba40f799bd5ded335
Virginia Gichira <gichiravirginia@gmail.com>
2024-06-09 03:32:09 +0300

    Use SQL statements

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Black', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'White', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Black', 'Melancholic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'Brown', 'Phlegmatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Dark Brown', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'F', 'Blond', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Cream', 'Phlegmatic', 1);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (4, 'M', 'Dark Brown', 'Phlegmatic', 1);
